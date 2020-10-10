bafed3cc51bf8328ae169de6c6ddc244eb3e12f9
Mykhailo Palahuta <michaeller.2012@gmail.com>
2020-10-10 21:19:51 +0300

    Secured feedbacks endpoint

--- src/main/java/ua/ozzy/apiback/controller/FeedbackController.java
+++ src/main/java/ua/ozzy/apiback/controller/FeedbackController.java
@@ -3,6 +3,7 @@ package ua.ozzy.apiback.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ public class FeedbackController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Page<FeedbackDto>> getFeedbacks(Pageable pageable) {
         Page<Feedback> feedbacks = feedbackService.getFeedbacks(pageable);
         Page<FeedbackDto> feedbackDtos = feedbacks.map(feedbackDtoMapper::toDto);
