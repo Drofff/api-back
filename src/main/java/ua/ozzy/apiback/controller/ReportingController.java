d28405c3f488c27440da58b9ebb0e96dd67158b9
Mykhailo Palahuta <michaeller.2012@gmail.com>
2020-12-26 01:30:34 +0200

    Statuses reporting job triggering endpoint

--- src/main/java/ua/ozzy/apiback/controller/ReportingController.java
+++ src/main/java/ua/ozzy/apiback/controller/ReportingController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.ozzy.apiback.dto.MessageDto;
 import ua.ozzy.apiback.dto.UserReportDto;
+import ua.ozzy.apiback.job.ReportingJob;
 import ua.ozzy.apiback.mapper.UserReportDtoMapper;
 import ua.ozzy.apiback.model.TelegramUser;
 import ua.ozzy.apiback.model.UserReport;
@@ -21,13 +23,15 @@ public class ReportingController {
 
     private final ReportingService reportingService;
     private final TelegramUserService telegramUserService;
+    private final ReportingJob reportingJob;
 
     private final UserReportDtoMapper userReportDtoMapper;
 
     public ReportingController(ReportingService reportingService, TelegramUserService telegramUserService,
-                               UserReportDtoMapper userReportDtoMapper) {
+                               ReportingJob reportingJob, UserReportDtoMapper userReportDtoMapper) {
         this.reportingService = reportingService;
         this.telegramUserService = telegramUserService;
+        this.reportingJob = reportingJob;
         this.userReportDtoMapper = userReportDtoMapper;
     }
 
@@ -38,6 +42,13 @@ public class ReportingController {
         UserReport userReport = reportingService.getReportForUser(user);
         UserReportDto reportDto = userReportDtoMapper.toDto(userReport);
         return ok(reportDto);
+    }
+
+    @GetMapping("/statuses")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<MessageDto> triggerStatusesReport() {
+        reportingJob.sendStatusReports();
+        return ok(new MessageDto("Started sending status reports"));
     }
 
 }
